e813202606f80461687a4944e68ca63fb1481147
Hyeongchae Son <3076270s@gmail.com>
2024-07-14 03:11:06 +0900

    Day 17 finished

--- Project 1/Assets.xcassets/DetailViewController.swift
+++ Project 1/Assets.xcassets/DetailViewController.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
     /*
     // MARK: - Navigation
 
